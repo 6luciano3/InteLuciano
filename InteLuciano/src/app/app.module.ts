4b63db35d47d4cf396d25d1c889e4c35f78c66fa
6luciano3 <99690369+6luciano3@users.noreply.github.com>
2022-08-11 20:43:02 -0300

    Con fondo completo

--- InteLuciano/src/app/app.module.ts
+++ InteLuciano/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './compenents/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
